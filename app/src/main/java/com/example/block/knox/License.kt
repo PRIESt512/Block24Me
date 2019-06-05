9336fc0d488a84caf98f691b15448b04fae581a0
aleksejpleskanev <priestman0@gmail.com>
2019-06-05 23:20:02 +0300

    Добавил включение/выключение в "Настроки" активации KNOX

--- app/src/main/java/com/example/block/knox/License.kt
+++ app/src/main/java/com/example/block/knox/License.kt
@@ -2,6 +2,6 @@ package com.example.block.knox
 
 object License {
     fun getLicenseCode(): String {
-        return "KLM06-65D9B-T59GT-41885-TTD52-ZZYWR"
+        return "KLM06-G7SVY-8JJVL-S3P51-Q99U2-L87IP"
     }
 }
